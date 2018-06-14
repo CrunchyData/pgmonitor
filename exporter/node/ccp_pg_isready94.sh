2a701008a0d608cff62c76525ab3403517f92918
Yogesh Sharma <Yogesh.Sharma@CrunchyData.com>
2018-06-14 10:36:42 -0700

    New metrics 1. Add log event counting 2. Add checksum capturing

--- exporter/node/ccp_pg_isready94.sh
+++ exporter/node/ccp_pg_isready94.sh
@@ -1,23 +1,92 @@
 #!/usr/bin/env bash
 
-PGDIR=/var/lib/ccp_monitoring/node_exporter
-PGTMP=is_pgready-9.4.tmp
-PGFILE=is_pgready-9.4.prom
-PGBIN=/usr/pgsql-9.4/bin
-if [ ! -d ${PGDIR} ]; then
-  mkdir ${PGDIR}
-fi
-rm -f ${PGDIR}/${PGTMP}
-$PGBIN/pg_isready -d postgres >/dev/null
-EX_VAL=$?
-if [ $EX_VAL -eq  0 ]; then
-	REC=$(psql -d postgres -Atc 'select pg_is_in_recovery()')
-	if [ "$REC" = "f" ]; then
-		echo "ccp_pg_ready 2" >>${PGDIR}/${PGTMP}
-	else
-		echo "ccp_pg_ready 1" >>${PGDIR}/${PGTMP}
-	fi
+P_LEVELS=('WARNING' 'ERROR' 'FATAL')
+P_COUNTS=(0 0 0)
+P_DIR=/var/lib/ccp_monitoring/node_exporter
+P_TMP=is_pgready-9.4.tmp
+P_FILE=is_pgready-9.4.prom
+P_BIN=/usr/pgsql-9.4/bin
+P_DATA=${PGDATA:-/var/lib/pgsql/9.6/data}
+P_FREQUENCY=5 #In minutes
+#################################
+
+isReady() {
+  $P_BIN/pg_isready -d postgres >/dev/null
+  EX_VAL=$?
+  if [ $EX_VAL -eq  0 ]; then
+    REC=$(psql -d postgres -Atc 'select pg_is_in_recovery()')
+    if [ "$REC" = "f" ]; then
+      echo "ccp_pg_ready 2" >>${P_DIR}/${P_TMP}
+    else
+      echo "ccp_pg_ready 1" >>${P_DIR}/${P_TMP}
+    fi
+  else
+    echo "ccp_pg_ready 0" >>${P_DIR}/${P_TMP}
+  fi
+}
+
+isEvent() {
+
+P_FILE_TC=${P_DIR}/tailncount.txt
+[ -f ${P_FILE_TC} ] && . ${P_FILE_TC}
+P_O_LOGFILE=${LAST_LOGFILE:-""}
+P_O_OFFSET=${LAST_OFFSET:-0}
+
+LOGDIR=$(psql -d postgres -Atc 'show log_directory;')
+LOGFILE=$(psql -d postgres -Atc 'show log_filename;')
+LDSTART=${LOGDIR:0:1}
+P_FILENAME=$(date -d "${P_FREQUENCY} minutes ago" "+${LOGFILE}")
+if [[ LDSTART == '/' ]] ; then
+  P_LOGFILE=${LOGDIR}/$P_FILENAME
 else
-	echo "ccp_pg_ready 0" >>${PGDIR}/${PGTMP}
+  P_LOGFILE=${P_DATA}/${LOGDIR}/$P_FILENAME
 fi
-mv ${PGDIR}/${PGTMP} ${PGDIR}/${PGFILE}
+
+P_OFFSET=$(stat -c%s $P_LOGFILE)
+if [[ $P_LOGFILE = $P_O_LOGFILE ]]; then
+  TO_READ=$(( P_OFFSET - P_O_OFFSET ))
+else
+  for i in ${!P_LEVELS[@]}
+  do
+    echo ${P_LEVELS[$i]} ${P_COUNTS[$i]}
+    P_COUNTS[$i]=$(tail -c +${P_O_OFFSET} ${P_O_LOGFILE} |grep "${P_LEVELS[$i]}" | wc -l)
+  done
+  TO_READ=$P_OFFSET
+  P_O_OFFSET=0
+fi
+
+for i in ${!P_LEVELS[@]}
+do
+  NCOUNT=$(tail -c +${P_O_OFFSET} ${P_LOGFILE} | head -c ${TO_READ} |grep "${P_LEVELS[$i]}" | wc -l)
+  OCOUNT=P_COUNTS[$i]
+  COUNT=$(( NCOUNT + OCOUNT ))
+  echo "ccp_log_event_count{event_type=\"${LEVEL}\"} ${COUNT}" >>${P_DIR}/${P_TMP}
+done
+
+cat >${P_FILE_TC} <<P_EOF
+LAST_LOGFILE=${P_LOGFILE}
+LAST_OFFSET=${P_OFFSET}
+P_EOF
+
+}
+
+isSettings() {
+  checksum=$(psql -d postgres -Atc 'SELECT name, setting FROM pg_settings ORDER BY name' | sha1sum | awk '{print $1}')
+  echo "ccp_config_checksum ${checksum}" >>${P_DIR}/${P_TMP}
+}
+
+#################################
+if [ ! -d ${P_DIR} ]; then
+  mkdir ${P_DIR}
+fi
+rm -f ${P_DIR}/${P_TMP}
+#################################
+
+isReady()
+isEvent()
+isSettings()
+
+#################################
+mv ${P_DIR}/${P_TMP} ${P_DIR}/${P_FILE}
+#################################
+
