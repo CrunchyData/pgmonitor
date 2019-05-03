4b9a6bdce086ff7bbd26a776d8e204584cf8aaa5
Yogesh Sharma <sharmay@users.noreply.github.com>
2019-05-03 09:25:05 -0700

    Add new Overview desktop for easy access (#97)
    * Add new Overview desktop for easy access
    * Fix refresh in PGBackrest dashboard
    * Add flag BACKREST_AUTOCONFIG_STANZAS

--- exporter/postgres/pgbackrest-info.sh
+++ exporter/postgres/pgbackrest-info.sh
@@ -2,7 +2,13 @@
 
 [ -f /etc/pgmonitor.conf ] && . /etc/pgmonitor.conf
 
+if [ ${BACKREST_AUTOCONFIG_STANZAS:-0} -gt 0 ]; then
+  BACKREST_STANZAS=$(grep '^\[' /etc/pgbackrest.conf /etc/pgbackrest/  -rh |sort -u |grep -v ':\|global' |sed 's/\[\|\]//g')
+  BACKREST_CONFIGS=""
+fi
+
 conf="default"
+
 if [ -z "$BACKREST_CONFIGS" ] && [ -z "$BACKREST_STANZAS" ]; then
     echo $(echo -n "$conf|" | tr '/' '_'; pgbackrest --output=json info | tr -d '\n')
 elif [ ! -z "$BACKREST_CONFIGS" ] && [ -z "$BACKREST_STANZAS" ]; then
@@ -17,21 +23,6 @@ elif [ -z "$BACKREST_CONFIGS" ] && [ ! -z "$BACKREST_STANZAS" ]; then
     do
       export PGBACKREST_STANZA=$stanza
       echo $(echo -n "$conf|" | tr '/' '_'; pgbackrest --output=json info | tr -d '\n')
-    done
-else
-    IFS=':' read -r -a config_array <<< "$BACKREST_CONFIGS"
-    IFS=':' read -r -a stanza_array <<< "$BACKREST_STANZAS"
-    if [ ${#config_array[@]} -ne ${#stanza_array[@]} ]; then
-      echo "Configuration error, BACKREST_CONFIGS and BACKREST_STANZAS must have same number of elements"
-      exit 1
-    fi
-    ary_len=${#config_array[@]}
-    for (( i=0; i<$ary_len; i++ ))
-    do
-      conf=${config_array[$i]}
-      stanza=${stanza_array[$i]}
-      export PGBACKREST_STANZA=$stanza
-      echo $(echo -n "$conf|" | tr '/' '_'; pgbackrest --config=$conf --output=json info | tr -d '\n')
     done
 fi
 
