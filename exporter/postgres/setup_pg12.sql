935e1f24fb38875a101aeca5fd5078534d1b42a7
Keith Fiske <keith@keithf4.com>
2020-01-22 10:06:42 -0500

    Add uptime/restart and checksum monitoring

--- exporter/postgres/setup_pg12.sql
+++ exporter/postgres/setup_pg12.sql
@@ -58,7 +58,7 @@ $function$;
 
 DROP FUNCTION IF EXISTS monitor.sequence_status();
 CREATE FUNCTION monitor.sequence_status() RETURNS TABLE (sequence_name text, last_value bigint, slots numeric, used numeric, percent int, cycle boolean, numleft numeric, table_usage text)  
-    LANGUAGE sql SECURITY DEFINER
+    LANGUAGE sql SECURITY DEFINER STABLE
 AS $function$
 
 /* 
@@ -115,7 +115,7 @@ $function$;
 
 DROP FUNCTION IF EXISTS monitor.sequence_exhaustion(int);
 CREATE FUNCTION monitor.sequence_exhaustion(p_percent integer DEFAULT 75, OUT count bigint)
- LANGUAGE sql SECURITY DEFINER
+ LANGUAGE sql SECURITY DEFINER STABLE
 AS $function$
 
 /* 
@@ -129,6 +129,142 @@ FROM (
     FROM pg_catalog.pg_sequences s
 ) x 
 WHERE (ROUND(used/slots*100)::int) > p_percent;
+
+$function$;
+
+
+/*
+ * Table and functions for monitoring changes to pg_settings and pg_hba_file_rules system catalogs.
+ * Can't just do a raw check for the hash value since Prometheus only records numeric values for alerts
+ * Table allows recording of existing settings so it can be referred back to to see what changed
+ * If function returns 0, then NO settings have changed for either pg_settings or hba since last known valid state 
+ * If function returns 1, then just pg_settings have changed since last known valid state
+ * If function returns 2, then just hba has changed since last known valid state
+ * If function returns 3, then both pg_settings and hba have changed since last known valid state
+ */
+
+DROP TABLE IF EXISTS monitor.settings_checksum;
+DROP FUNCTION IF EXISTS monitor.settings_checksum();
+
+CREATE TABLE monitor.settings_checksum (
+    settings_hash text NOT NULL
+    , settings_string text NOT NULL
+    , hba_hash text NOT NULL
+    , hba_string text NOT NULL
+    , created_at timestamptz DEFAULT now() NOT NULL
+    , valid smallint NOT NULL );
+
+COMMENT ON COLUMN monitor.settings_checksum.valid IS 'Set this column to zero if this group of settings is a valid change';
+CREATE INDEX ON monitor.settings_checksum (created_at);
+
+CREATE FUNCTION monitor.settings_checksum () RETURNS smallint
+    LANGUAGE plpgsql SECURITY DEFINER 
+AS $function$
+DECLARE
+
+v_hba_hash              text;
+v_hba_hash_old          text;
+v_hba_match             smallint := 0;
+v_hba_string            text;
+v_hba_string_old        text;
+v_match_total           smallint := 0;
+v_settings_hash         text;
+v_settings_hash_old     text;
+v_settings_match        smallint := 0;
+v_settings_string       text;
+v_settings_string_old   text;
+v_valid                 smallint;
+
+BEGIN
+
+WITH settings_ordered_list AS (
+    SELECT name
+        , COALESCE(setting, '<<NULL>>') AS setting
+    FROM pg_catalog.pg_settings 
+    ORDER BY name, setting)
+SELECT md5(string_agg(name||setting, ',')) 
+    , string_agg(name||setting, ',')
+INTO v_settings_hash
+    , v_settings_string
+FROM settings_ordered_list;
+
+-- Order by line number so it's caught if no content is changed but the order of entries is changed
+WITH hba_ordered_list AS (
+    SELECT COALESCE(type, '<<NULL>>') AS type
+        , array_to_string(COALESCE(database, ARRAY['<<NULL>>']), ',') AS database
+        , array_to_string(COALESCE(user_name, ARRAY['<<NULL>>']), ',') AS user_name
+        , COALESCE(address, '<<NULL>>') AS address
+        , COALESCE(netmask, '<<NULL>>') AS netmask
+        , COALESCE(auth_method, '<<NULL>>') AS auth_method
+        , array_to_string(COALESCE(options, ARRAY['<<NULL>>']), ',') AS options
+    FROM pg_catalog.pg_hba_file_rules
+    ORDER BY line_number)
+SELECT md5(string_agg(type||database||user_name||address||netmask||auth_method||options, ','))
+    , string_agg(type||database||user_name||address||netmask||auth_method||options, ',')
+INTO v_hba_hash
+    , v_hba_string
+FROM hba_ordered_list;
+
+SELECT settings_hash, hba_hash, valid
+INTO v_settings_hash_old, v_hba_hash_old, v_valid
+FROM monitor.settings_checksum
+ORDER BY created_at DESC LIMIT 1;
+
+IF v_settings_hash_old IS NOT NULL THEN
+
+    IF v_valid > 0 OR (v_settings_hash != v_settings_hash_old OR v_hba_hash != v_hba_hash_old) THEN
+        
+        IF v_settings_hash != v_settings_hash_old OR v_valid = 1 THEN
+            v_settings_match := 1;
+        END IF;
+        
+        IF v_hba_hash != v_hba_hash_old OR v_valid = 2 THEN
+            v_hba_match := 2;
+        END IF;
+
+        IF v_valid = 3 THEN 
+            -- If previous state had both mismatched, ensure that is preserved
+            v_match_total = 3;
+        ELSE
+            -- Otherwise, ensure new state is set
+            v_match_total := v_settings_match + v_hba_match;
+        END IF;
+
+        -- Only insert a new row if one of the hashes has changed since last time
+        IF (v_settings_hash != v_settings_hash_old OR v_hba_hash != v_hba_hash_old) THEN 
+            INSERT INTO monitor.settings_checksum (settings_hash, settings_string, hba_hash, hba_string, valid)
+            VALUES (v_settings_hash, v_settings_string, v_hba_hash, v_hba_string, v_match_total);
+        END IF;
+
+    END IF; 
+
+ELSE
+
+    INSERT INTO monitor.settings_checksum (settings_hash, settings_string, hba_hash, hba_string, valid)
+    VALUES (v_settings_hash, v_settings_string, v_hba_hash, v_hba_string, v_match_total);
+
+END IF;
+
+RETURN v_match_total;
+
+END
+$function$;
+
+/*
+ * This function provides quick, clear interface for resetting the checksum monitor to treat the currently detected configuration as valid after alerting on a change.
+ */
+CREATE FUNCTION monitor.settings_checksum_set_valid() RETURNS void
+    LANGUAGE sql 
+AS $function$
+
+-- Should also handle edge case of there being multiple timestamps with same value
+-- All should be updated to ensure consistency in a weird state
+WITH max_time AS ( 
+    SELECT max(created_at) as max_created FROM monitor.settings_checksum ) 
+UPDATE monitor.settings_checksum 
+SET valid = 0 
+FROM max_time 
+WHERE created_at = max_time.max_created;
 
 $function$;
 
