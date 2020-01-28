ef3dfad80168c58c304dd3d345568cb8e5ee9011
Keith Fiske <keith@keithf4.com>
2020-01-28 18:32:03 -0500

    Make pg_settings and pg_hba checksum monitoring separate metrics.

--- exporter/postgres/setup_pg12.sql
+++ exporter/postgres/setup_pg12.sql
@@ -134,55 +134,50 @@ $function$;
 
 
 /*
- * Table and functions for monitoring changes to pg_settings and pg_hba_file_rules system catalogs.
+ * Tables and functions for monitoring changes to pg_settings and pg_hba_file_rules system catalogs.
  * Can't just do a raw check for the hash value since Prometheus only records numeric values for alerts
- * Table allows recording of existing settings so it can be referred back to to see what changed
- * If function returns 0, then NO settings have changed for either pg_settings or hba since last known valid state 
- * If function returns 1, then just pg_settings have changed since last known valid state
- * If function returns 2, then just hba has changed since last known valid state
- * If function returns 3, then both pg_settings and hba have changed since last known valid state
+ * Tables allow recording of existing settings so they can be referred back to to see what changed
+ * If either checksum function returns 0, then NO settings have changed 
+ * If either checksum function returns 1, then something has changed since last known valid state
  * For replicas, logging past settings is not possible to compare what may have changed
  * For replicas, by default, it is expected that its settings will match the primary
  * For replicas, if the pg_settings or pg_hba.conf are necessarily different from the primary, a known good hash of that replica's
-    settings can be sent as arguments to the settings_checksum() function. Views are provided to easily obtain hash values 
-    that this monitoring metric expects to use.
- * If any known hash parameters are passed to the settings_checksum() function, note that it will override any past hash values for both
-    pg_settings and hba stored in the log table when doing comparisons and completely re-evaluate the entire state. This is
-    true even if done on a primary where the current state will then also be logged for comparison.
+    settings can be sent as an argument to the relevant checksum function. Views are provided to easily obtain the hash values used by this monitoring tool. 
+ * If any known hash parameters are passed to the checksum functions, note that it will override any past hash values stored in the log table when doing comparisons and completely re-evaluate the entire state. This is true even if done on a primary where the current state will then also be logged for comparison if it differs from the given hash.
  */
 
-DROP TABLE IF EXISTS monitor.settings_checksum;
-DROP FUNCTION IF EXISTS monitor.settings_checksum(text, text);
-DROP FUNCTION monitor.settings_checksum_set_valid();
-DROP VIEW IF EXISTS monitor.pg_settings_hash;
-DROP VIEW IF EXISTS monitor.pg_hba_hash;
+DROP TABLE IF EXISTS monitor.pg_settings_checksum;
+DROP TABLE IF EXISTS monitor.pg_hba_checksum;
 
-CREATE TABLE monitor.settings_checksum (
+CREATE TABLE monitor.pg_settings_checksum (
     settings_hash_generated text NOT NULL
     , settings_hash_known_provided text
     , settings_string text NOT NULL
-    , hba_hash_generated text NOT NULL
+    , created_at timestamptz DEFAULT now() NOT NULL
+    , valid smallint NOT NULL );
+
+COMMENT ON COLUMN monitor.pg_settings_checksum.valid IS 'Set this column to zero if this group of settings is a valid change';
+CREATE INDEX ON monitor.pg_settings_checksum (created_at);
+
+CREATE TABLE monitor.pg_hba_checksum (
+    hba_hash_generated text NOT NULL
     , hba_hash_known_provided text
     , hba_string text NOT NULL
     , created_at timestamptz DEFAULT now() NOT NULL
     , valid smallint NOT NULL );
 
-COMMENT ON COLUMN monitor.settings_checksum.valid IS 'Set this column to zero if this group of settings is a valid change';
-CREATE INDEX ON monitor.settings_checksum (created_at);
+COMMENT ON COLUMN monitor.pg_hba_checksum.valid IS 'Set this column to zero if this group of settings is a valid change';
+CREATE INDEX ON monitor.pg_hba_checksum (created_at);
 
-CREATE FUNCTION monitor.settings_checksum(p_known_settings_hash text DEFAULT NULL, p_known_hba_hash text DEFAULT NULL) 
+
+DROP FUNCTION IF EXISTS monitor.pg_settings_checksum(text);
+CREATE FUNCTION monitor.pg_settings_checksum(p_known_settings_hash text DEFAULT NULL) 
     RETURNS smallint
     LANGUAGE plpgsql SECURITY DEFINER 
 AS $function$
 DECLARE
 
-v_hba_hash              text;
-v_hba_hash_old          text;
-v_hba_match             smallint := 0;
-v_hba_string            text;
-v_hba_string_old        text;
 v_is_in_recovery        boolean;
-v_match_total           smallint := 0;
 v_settings_hash         text;
 v_settings_hash_old     text;
 v_settings_match        smallint := 0;
@@ -200,6 +195,79 @@ INTO v_settings_hash
     , v_settings_string
 FROM monitor.pg_settings_hash;
 
+SELECT settings_hash_generated, valid
+INTO v_settings_hash_old, v_valid
+FROM monitor.pg_settings_checksum
+ORDER BY created_at DESC LIMIT 1;
+
+IF p_known_settings_hash IS NOT NULL THEN
+    v_settings_hash_old := p_known_settings_hash;
+    -- Do not base validity on the stored value if manual hash is given. 
+    v_valid := 0;
+END IF;
+
+IF (v_settings_hash_old IS NOT NULL) THEN
+
+    IF (v_settings_hash != v_settings_hash_old) THEN
+
+        v_valid := 1;
+
+        IF v_is_in_recovery = false THEN 
+            INSERT INTO monitor.pg_settings_checksum (
+                    settings_hash_generated
+                    , settings_hash_known_provided
+                    , settings_string
+                    , valid)
+            VALUES (
+                    v_settings_hash
+                    , p_known_settings_hash
+                    , v_settings_string
+                    , v_valid);
+        END IF;
+    END IF;
+
+ELSE
+
+    v_valid := 0;
+    IF v_is_in_recovery = false THEN
+        INSERT INTO monitor.pg_settings_checksum (
+                settings_hash_generated
+                , settings_hash_known_provided
+                , settings_string
+                , valid)
+        VALUES (v_settings_hash
+                , p_known_settings_hash
+                , v_settings_string
+                , v_valid);
+    END IF;
+
+END IF; 
+
+RETURN v_valid;
+
+END
+$function$;
+
+
+DROP FUNCTION IF EXISTS monitor.pg_hba_checksum(text);
+CREATE FUNCTION monitor.pg_hba_checksum(p_known_hba_hash text DEFAULT NULL) 
+    RETURNS smallint
+    LANGUAGE plpgsql SECURITY DEFINER 
+AS $function$
+DECLARE
+
+v_hba_hash              text;
+v_hba_hash_old          text;
+v_hba_match             smallint := 0;
+v_hba_string            text;
+v_hba_string_old        text;
+v_is_in_recovery        boolean;
+v_valid                 smallint;
+
+BEGIN
+
+SELECT pg_is_in_recovery() INTO v_is_in_recovery;
+
 IF current_setting('server_version_num')::int >= 100000 THEN
 
     SELECT md5_hash
@@ -209,119 +277,76 @@ IF current_setting('server_version_num')::int >= 100000 THEN
     FROM monitor.pg_hba_hash;
 
 ELSE
-    v_hba_hash := 'unsupported in versions older than PostgreSQL 10';
-    v_hba_string := 'unsupported in versions older than PostgreSQL 10';
+    RAISE EXCEPTION 'pg_hba change monitoring unsupported in versions older than PostgreSQL 10';
 END IF;
 
-SELECT settings_hash_generated, hba_hash_generated, valid
-INTO v_settings_hash_old, v_hba_hash_old, v_valid
-FROM monitor.settings_checksum
+SELECT  hba_hash_generated, valid
+INTO v_hba_hash_old, v_valid
+FROM monitor.pg_hba_checksum
 ORDER BY created_at DESC LIMIT 1;
 
 IF p_known_hba_hash IS NOT NULL THEN
-    IF current_setting('server_version_num')::int >= 100000 THEN
-        v_hba_hash_old := p_known_hba_hash;
-    ELSE 
-        RAISE EXCEPTION 'p_known_hba_hash parameter can only be used with PG10+';
-    END IF;
-END IF;
-
-IF p_known_settings_hash IS NOT NULL THEN
-    v_settings_hash_old := p_known_settings_hash;
-END IF;
-
-IF v_is_in_recovery THEN
-    -- Do not base validity on the stored value on primary. 
-    -- Allow it to be re-evaluated on the replica itself
+    v_hba_hash_old := p_known_hba_hash;
+    -- Do not base validity on the stored value if manual hash is given. 
     v_valid := 0;
 END IF;
 
--- Check for NOT NULL in v_settings_hash_old since that will be set both when a manual known hash is given
---      or the last stored one from the log file is used. Otherwise, must insert new row in history table.
-IF v_settings_hash_old IS NOT NULL THEN
+IF (v_hba_hash_old IS NOT NULL) THEN
 
-    IF v_valid > 0 OR (v_settings_hash != v_settings_hash_old OR v_hba_hash != v_hba_hash_old) THEN
-        
-        IF v_settings_hash != v_settings_hash_old OR (v_valid = 1 AND p_known_settings_hash IS NULL) THEN
-            v_settings_match := 1;
-        END IF;
-        
-        IF v_hba_hash != v_hba_hash_old OR (v_valid = 2 AND p_known_hba_hash IS NULL )THEN
-            v_hba_match := 2;
-        END IF;
+    IF (v_hba_hash != v_hba_hash_old) THEN
 
-        IF (p_known_settings_hash IS NOT NULL OR p_known_hba_hash IS NOT NULL) THEN
-            -- If known hash values are being provided, always re-evaluate state
-            v_match_total := v_settings_match + v_hba_match;
-        ELSIF v_valid = 3 THEN
-            -- If previous state had both mismatched on the primary, and no known hashes are provided, preserve that state
-            v_match_total = 3;
-        ELSE
-            -- Otherwise, ensure new state is set
-            v_match_total := v_settings_match + v_hba_match;
-        END IF;
+        v_valid := 1;
 
-        -- Only insert a new row if one of the hashes has changed since last time (and on primary)
-        IF v_is_in_recovery = false AND (v_settings_hash != v_settings_hash_old OR v_hba_hash != v_hba_hash_old) THEN 
-            INSERT INTO monitor.settings_checksum (
-                    settings_hash_generated
-                    , settings_hash_known_provided
-                    , settings_string
-                    , hba_hash_generated
+        IF v_is_in_recovery = false THEN 
+            INSERT INTO monitor.pg_hba_checksum (
+                    hba_hash_generated
                     , hba_hash_known_provided
                     , hba_string
                     , valid)
-            VALUES (v_settings_hash
-                    , p_known_settings_hash
-                    , v_settings_string
-                    , v_hba_hash
+            VALUES (
+                    v_hba_hash
                     , p_known_hba_hash
                     , v_hba_string
-                    , v_match_total);
+                    , v_valid);
         END IF;
-
-    END IF; 
+    END IF;
 
 ELSE
 
+    v_valid := 0;
     IF v_is_in_recovery = false THEN
-        INSERT INTO monitor.settings_checksum (
-                settings_hash_generated
-                , settings_hash_known_provided
-                , settings_string
-                , hba_hash_generated
+        INSERT INTO monitor.pg_hba_checksum (
+                hba_hash_generated
                 , hba_hash_known_provided
                 , hba_string
                 , valid)
-        VALUES (v_settings_hash
-                , p_known_settings_hash
-                , v_settings_string
-                , v_hba_hash
+        VALUES (v_hba_hash
                 , p_known_hba_hash
                 , v_hba_string
-                , v_match_total);
+                , v_valid);
     END IF;
 
-END IF;
+END IF; 
 
-RETURN v_match_total;
+RETURN v_valid;
 
 END
 $function$;
 
 
+DROP FUNCTION IF EXISTS monitor.pg_settings_checksum_set_valid();
 /*
  * This function provides quick, clear interface for resetting the checksum monitor to treat the currently detected configuration as valid  against the one stored in the history table after alerting on a change.
  */
-CREATE FUNCTION monitor.settings_checksum_set_valid() RETURNS void
+CREATE FUNCTION monitor.pg_settings_checksum_set_valid() RETURNS void
     LANGUAGE sql 
 AS $function$
 
 -- Should also handle edge case of there being multiple timestamps with same value
 -- All should be updated to ensure consistency in a weird state
 WITH max_time AS ( 
-    SELECT max(created_at) as max_created FROM monitor.settings_checksum ) 
-UPDATE monitor.settings_checksum 
+    SELECT max(created_at) as max_created FROM monitor.pg_settings_checksum ) 
+UPDATE monitor.pg_settings_checksum 
 SET valid = 0 
 FROM max_time 
 WHERE created_at = max_time.max_created;
@@ -329,6 +354,27 @@ WHERE created_at = max_time.max_created;
 $function$;
 
 
+DROP FUNCTION IF EXISTS monitor.pg_hba_checksum_set_valid();
+/*
+ * This function provides quick, clear interface for resetting the checksum monitor to treat the currently detected configuration as valid  against the one stored in the history table after alerting on a change.
+ */
+CREATE FUNCTION monitor.pg_hba_checksum_set_valid() RETURNS void
+    LANGUAGE sql 
+AS $function$
+
+-- Should also handle edge case of there being multiple timestamps with same value
+-- All should be updated to ensure consistency in a weird state
+WITH max_time AS ( 
+    SELECT max(created_at) as max_created FROM monitor.pg_hba_checksum ) 
+UPDATE monitor.pg_hba_checksum 
+SET valid = 0 
+FROM max_time 
+WHERE created_at = max_time.max_created;
+
+$function$;
+
+
+DROP VIEW IF EXISTS monitor.pg_settings_hash;
 CREATE VIEW monitor.pg_settings_hash AS
     WITH settings_ordered_list AS (
         SELECT name
@@ -340,6 +386,7 @@ CREATE VIEW monitor.pg_settings_hash AS
     FROM settings_ordered_list;
 
 
+DROP VIEW IF EXISTS monitor.pg_hba_hash;
 CREATE VIEW monitor.pg_hba_hash AS
     -- Order by line number so it's caught if no content is changed but the order of entries is changed
     WITH hba_ordered_list AS (
