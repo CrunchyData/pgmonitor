5e431602c868c29c2269519275c0db540aafd092
Yogesh Sharma <Yogesh.Sharma@CrunchyData.com>
2021-02-23 15:25:16 -0800

    Convert newline to space Special  IFS is not reuired

--- postgres_exporter/linux/pgbackrest-info.sh
+++ postgres_exporter/linux/pgbackrest-info.sh
@@ -8,7 +8,7 @@
 [ -f /etc/pgmonitor.conf ] && . /etc/pgmonitor.conf
 
 if [ ${BACKREST_AUTOCONFIG_STANZAS:-0} -gt 0 ]; then
-  BACKREST_STANZAS=$(grep '^\[' /etc/pgbackrest.conf /etc/pgbackrest/  -rh |sort -u |grep -v ':\|global' |sed 's/\[\|\]//g')
+  BACKREST_STANZAS=$(grep '^\[' /etc/pgbackrest.conf /etc/pgbackrest/  -rh |sort -u |grep -v ':\|global' |sed 's/\[\|\]//g' | tr '\n' ' ' )
   BACKREST_CONFIGS=""
 fi
 
@@ -17,13 +17,13 @@ conf="default"
 if [ -z "$BACKREST_CONFIGS" ] && [ -z "$BACKREST_STANZAS" ]; then
     echo $(echo -n "$conf|" | tr '/' '_'; pgbackrest --output=json info | tr -d '\n')
 elif [ ! -z "$BACKREST_CONFIGS" ] && [ -z "$BACKREST_STANZAS" ]; then
-    IFS=':' read -r -a config_array <<< "$BACKREST_CONFIGS"
+    read -r -a config_array <<< "$BACKREST_CONFIGS"
     for conf in "${config_array[@]}"
     do
       echo $(echo -n "$conf|" | tr '/' '_'; pgbackrest --config=$conf --output=json info | tr -d '\n')
     done
 elif [ -z "$BACKREST_CONFIGS" ] && [ ! -z "$BACKREST_STANZAS" ]; then
-    IFS=':' read -r -a stanza_array <<< "$BACKREST_STANZAS"
+    read -r -a stanza_array <<< "$BACKREST_STANZAS"
     for stanza in "${stanza_array[@]}"
     do
       export PGBACKREST_STANZA=$stanza
