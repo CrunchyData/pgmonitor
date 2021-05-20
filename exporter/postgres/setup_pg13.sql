c99135eb04d3052bc039c1850ce299ec0bbb9c9f
Keith Fiske <keith@keithf4.com>
2021-05-20 10:39:38 -0400

    Fix search path (#242)
    * Fix search path
    
    * Updated release notes
    
    Co-authored-by: Yogesh Sharma <sharmay@users.noreply.github.com>

--- exporter/postgres/setup_pg13.sql
+++ exporter/postgres/setup_pg13.sql
@@ -7,6 +7,7 @@ END
 $$;
  
 GRANT pg_monitor to ccp_monitoring;
+GRANT pg_execute_server_program TO ccp_monitoring;
 
 ALTER ROLE ccp_monitoring SET lock_timeout TO '2min';
 
@@ -20,7 +21,8 @@ ALTER TABLE monitor.pgbackrest_info SET (autovacuum_analyze_scale_factor = 0, au
 DROP FUNCTION IF EXISTS monitor.pgbackrest_info(); -- old version from 2.3
 DROP FUNCTION IF EXISTS monitor.pgbackrest_info(int);
 CREATE OR REPLACE FUNCTION monitor.pgbackrest_info(p_throttle_minutes int DEFAULT 10) RETURNS SETOF monitor.pgbackrest_info
-    LANGUAGE plpgsql SECURITY DEFINER
+    LANGUAGE plpgsql
+    SET search_path TO pg_catalog, pg_temp
 AS $function$
 DECLARE
 
@@ -59,6 +61,7 @@ $function$;
 DROP FUNCTION IF EXISTS monitor.sequence_status();
 CREATE FUNCTION monitor.sequence_status() RETURNS TABLE (sequence_name text, last_value bigint, slots numeric, used numeric, percent int, cycle boolean, numleft numeric, table_usage text)  
     LANGUAGE sql SECURITY DEFINER STABLE
+    SET search_path TO pg_catalog, pg_temp
 AS $function$
 
 /* 
@@ -115,7 +118,8 @@ $function$;
 
 DROP FUNCTION IF EXISTS monitor.sequence_exhaustion(int);
 CREATE FUNCTION monitor.sequence_exhaustion(p_percent integer DEFAULT 75, OUT count bigint)
- LANGUAGE sql SECURITY DEFINER STABLE
+    LANGUAGE sql SECURITY DEFINER STABLE
+    SET search_path TO pg_catalog, pg_temp
 AS $function$
 
 /* 
@@ -173,6 +177,7 @@ DROP FUNCTION IF EXISTS monitor.pg_settings_checksum(text);
 CREATE FUNCTION monitor.pg_settings_checksum(p_known_settings_hash text DEFAULT NULL) 
     RETURNS smallint
     LANGUAGE plpgsql SECURITY DEFINER 
+    SET search_path TO pg_catalog, pg_temp
 AS $function$
 DECLARE
 
@@ -252,6 +257,7 @@ DROP FUNCTION IF EXISTS monitor.pg_hba_checksum(text);
 CREATE FUNCTION monitor.pg_hba_checksum(p_known_hba_hash text DEFAULT NULL) 
     RETURNS smallint
     LANGUAGE plpgsql SECURITY DEFINER 
+    SET search_path TO pg_catalog, pg_temp
 AS $function$
 DECLARE
 
