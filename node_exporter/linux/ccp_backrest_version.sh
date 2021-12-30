60a04f7ccd60a458aa7fab57f577749d0617245e
Keith Fiske <keith@keithf4.com>
2021-12-30 17:39:59 -0500

    Fix prom textfile format for backrest version check

--- node_exporter/linux/ccp_backrest_version.sh
+++ node_exporter/linux/ccp_backrest_version.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 ###
 #
-# Copyright 2017-2021 Crunchy Data Solutions, Inc. All Rights Reserved.
+# Copyright 2017-2022 Crunchy Data Solutions, Inc. All Rights Reserved.
 #
 ###
 
-# Additional metric option to monitor disk queue depth with node_exporter. Set the DISK variable to the disk system that is to be monitored
+# Additional metric option to monitor pgBackRest version
 
 OUTPUTDIR=/var/lib/ccp_monitoring/node_exporter
-VERTMP=backrest_version.tmp
-VERFILE=backrest_version.prom
+VERTMP=ccp_backrest_version.tmp
+VERFILE=ccp_backrest_version.prom
 if [ ! -d ${OUTPUTDIR} ]; then
   mkdir ${OUTPUTDIR}
 fi
 rm -f ${OUTPUTDIR}/${VERTMP}
 cat <<EOF >${OUTPUTDIR}/${VERTMP}
-HELP ccp_backrest_version The version of pgBackRest installed on this system
-TYPE ccp_backrest_version gauge
+# HELP ccp_backrest_version The version of pgBackRest installed on this system
+# TYPE ccp_backrest_version gauge
 ccp_backrest_version{} $(pgbackrest version | cut -d ' ' -f 2)
 EOF
 mv ${OUTPUTDIR}/${VERTMP} ${OUTPUTDIR}/${VERFILE}
