e46796605faf4e6f02bf363593b08c9634f31a87
Keith Fiske <keith@keithf4.com>
2022-01-05 10:54:04 -0500

    Update backrest version check script

--- node_exporter/linux/ccp_backrest_version.sh
+++ node_exporter/linux/ccp_backrest_version.sh
@@ -8,15 +8,15 @@
 # Additional metric option to monitor pgBackRest version
 
 OUTPUTDIR=/var/lib/ccp_monitoring/node_exporter
-VERTMP=ccp_backrest_version.tmp
+VERTMP="$(mktemp ${OUTPUTDIR}/ccp_backrest_versionXXX)"
 VERFILE=ccp_backrest_version.prom
 if [ ! -d ${OUTPUTDIR} ]; then
   mkdir -p ${OUTPUTDIR}
 fi
-rm -f ${OUTPUTDIR}/${VERTMP}
-cat <<EOF >${OUTPUTDIR}/${VERTMP}
+cat <<EOF >${VERTMP}
 # HELP ccp_backrest_version The version of pgBackRest installed on this system
 # TYPE ccp_backrest_version gauge
-ccp_backrest_version{} $(pgbackrest version | cut -d ' ' -f 2)
+ccp_backrest_version{} $(pgbackrest version | cut -s -d ' ' -f 2)
 EOF
-mv ${OUTPUTDIR}/${VERTMP} ${OUTPUTDIR}/${VERFILE}
+mv ${VERTMP} ${OUTPUTDIR}/${VERFILE}
+
