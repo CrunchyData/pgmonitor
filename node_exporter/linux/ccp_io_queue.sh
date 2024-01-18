7fe584bbf0de07d083e161f02233f18980ac1382
David Youatt <david.youatt@crunchydata.com>
2024-01-18 13:47:51 -0800

    update copyright range to include 2024 (#392)

--- node_exporter/linux/ccp_io_queue.sh
+++ node_exporter/linux/ccp_io_queue.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ###
 #
-# Copyright © 2017-2023 Crunchy Data Solutions, Inc. All Rights Reserved.
+# Copyright © 2017-2024 Crunchy Data Solutions, Inc. All Rights Reserved.
 #
 ###
 
